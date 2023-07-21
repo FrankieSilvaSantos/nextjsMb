b32715c919d4bc42f22863a483ad1ab34f50db7b
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-07-21 17:08:21 -0300

    404Styles

--- app/todos/404/page.js
+++ app/todos/404/page.js
@@ -1,13 +1,19 @@
 import Link from 'next/link'
- 
+import {BiSolidInvader} from 'react-icons/bi' 
+import styles from './404.module.css'
+
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <p>
-        View <Link href="/">all posts</Link>
-      </p>
+    <div className={`${styles.containerNotFound}`}>
+      <div>
+      <div className={`alert alert-danger ${styles.error}`} role="alert">
+      <BiSolidInvader className={`${styles.iconNotFound}`}  ></BiSolidInvader>     404 Pagina não encontrada<Link href="/" className={`alert-link ${styles.linkStyle}`}> Volte para a HOME</Link>
+</div>
+      <h2 className={`${styles.tituloNotFound}`}> </h2>
+     
+      <section style={{height:'27vh'}}></section> 
+    </div>
     </div>
   )
 }
